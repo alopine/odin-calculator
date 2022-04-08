285eb0b4bbe5645cb36b2474d391efdcbb141a45
alopine <loariel98@gmail.com>
2022-04-07 18:36:02 -0700

    Overhaul calculator logic

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // Global Variables
-let displayValue;
-let currentOperator;
-let inputA;
-let inputB;
-let operatorActive = false;
-let inputMode = "A";
+let firstOperand = "";
+let secondOperand = "";
+let currentOperator = null;
+let total = null;
+let resetDisplay = false;
 
 
 // HTML Selectors
@@ -14,67 +13,88 @@ const numButtons = document.querySelectorAll(".numButton");
 const opButtons = document.querySelectorAll(".opButton");
 const calcButton = document.querySelector("#calcButton");
 
+display.textContent = null;
 
 // Calculator Functions
 function operate(a, operator, b) {
+    a = Number(a);
+    b = Number(b);
     switch(true) {
         case (operator === "+"):
             return a + b;
-        case (operator === "-"):
+        case (operator === "−"):
             return a - b;
-        case (operator === "*"):
+        case (operator === "×"):
             return a * b;
-        case (operator === "/"):
-            return a / b;
+        case (operator === "÷"):
+            if (b === 0) {
+                alert("You can't divide by 0!");
+                return 0;
+            } else{
+                return a / b;
+            }
     }
-}
-
-function updateDisplay(number) {
-    display.textContent += number;
-    displayValue = parseInt(display.textContent);
 }
 
 function clearDisplay() {
     display.textContent = null;
-    displayValue = null;
-    currentOperator = null;
-    inputA = null;
-    inputB = null;
-    operatorActive = false;
+    resetDisplay = false;
 }
 
+function clearAll() {
+    clearDisplay();
+    firstOperand = null;
+    secondOperand = null;
+    currentOperator = null;
+    total = null;
+}
+
+function updateDisplay(number) {
+    if (display.textContent === null || display.textContent === "0") {
+        clearDisplay();
+    }
+    display.textContent += number;
+}
+
+function calculate() {
+    if (!currentOperator || resetDisplay){
+        return;
+    } else {
+        secondOperand = display.textContent;
+        total = operate(firstOperand, currentOperator, secondOperand);
+        currentOperator = null;
+        firstOperand = total;
+        secondOperand = null;
+        clearDisplay();
+        updateDisplay(total);
+    }
+}
 
 // Event Listeners
 clearButton.addEventListener("click", () => {
-    clearDisplay();
+    clearAll();
 });
 
 numButtons.forEach((button) => {
     button.addEventListener("click", () => {
-        if (operatorActive && (inputMode !== "B")) {
-            display.textContent = null;
-            displayValue = null;
-            inputMode = "B"
-        }
+        if (resetDisplay === true) {
+            clearDisplay();
+        } 
         updateDisplay(button.textContent);
-        console.log(inputA, currentOperator, inputB);
     });
 }); 
 
 opButtons.forEach((button) => {
     button.addEventListener("click", () => {
+        if (currentOperator) {
+            calculate();
+        }
         currentOperator = button.textContent;
-        inputA = displayValue;
-        operatorActive = true;
-        console.log(inputA, currentOperator, inputB);
+        firstOperand = display.textContent;
+        resetDisplay = true;
     });
 });
 
 calcButton.addEventListener("click", () => {
-        inputB = displayValue;
-        display.textContent = null;
-        displayValue = null;
-        updateDisplay(operate(inputA, currentOperator, inputB));
-        console.log(inputA, currentOperator, inputB);
-        inputMode = "A";
+    calculate();
 });
